3e04c3a04087e096b004c00370d05eea59a0b171
Heba106 <96366380+Heba106@users.noreply.github.com>
2022-03-04 12:57:22 +0200

    Add files via upload

---
+++ Task2-Gradient-Descent-Linear-Regression/main.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import csv
+import random
+
+x = []
+y = []
+with open('dataset.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    csv_reader = csv.DictReader(csv_file)
+    for i in csv_reader:
+        x.append(int(i['x']))
+        y.append(float(i['y']))
+m=len(y)
+def hypothesis(x, theta0, theta1):
+    return theta0*theta1*x
+
+def cost(theta0, theta1,x,y):
+    sum = 0
+    for i in range(m):
+        sum += (hypothesis(x[i], theta0, theta1) - y[i])**2
+    return sum/2*m
+
+def gradientDescent(theta0, theta1, alpha):
+    sum_theta0 = 0
+    for i in range(m):
+        sum_theta0 += (hypothesis(x[i], theta0, theta1) - y[i])
+
+    sum_theta1 = 0
+    for i in range(0, m):
+        sum_theta1 += (hypothesis(x[i], theta0, theta1) - y[i])*x[i]
+
+    theta0 = theta0 - alpha * sum_theta0/m
+    theta1 = theta1 - alpha * sum_theta1/m
+    return theta0, theta1
+
+if __name__ == "__main__":
+    theta0 = random.random()
+    theta1 = random.random()
+    alpha = 0.0002
+
+    for i in range(100):   
+        theta0, theta1 = gradientDescent(theta0, theta1, alpha)
+    print("theta0 \t\t theta1")
+    print(theta0,theta1)
+    
+    # plotting:
+    plt.plot(x, y , 'x')
+    result=[]
+    for i in x:
+        result.append(hypothesis(i, theta0, theta1))
+    plt.plot(x, result ,color = "red")
+    plt.show()
